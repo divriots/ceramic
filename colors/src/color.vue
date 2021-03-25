d4f9f5d1d10baa198cebfffa0a8aa30102567041
Gregory Quinio <quinio.gregory@gmail.com>
2021-03-25 20:56:25 +0000

    Add stories and colors tokens

---
+++ colors/src/color.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+  props:{
+    name: String,
+    value: String,
+    hex: String
+  }
+}
+</script>
+<template>
+  <div class="space-y-1.5">
+    <div class="h-10 w-full rounded ring-1 ring-inset ring-black ring-opacity-0" :style="`background-color: ${value}`">
+    </div>
+    <div class="px-0.5 md:flex md:justify-between md:space-x-2 2xl:space-x-0 2xl:block">
+      <div class="w-6 font-medium text-gray-900">{{name}}</div>
+      <div>{{hex}}</div>
+    </div>
+  </div>
+</template>
