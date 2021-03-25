f89e3a0767cf3ba55a0ca88eca5189e2e473abc2
Gregory Quinio <quinio.gregory@gmail.com>
2021-03-25 10:41:25 +0000

    add promise

---
+++ promise/src/group.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+  },
+};
+</script>
+<template>
+  <h3 class="text-center text-4xl mt-20 mb-10">{{ title }}</h3>
+</template>
