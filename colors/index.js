6b9eb3dcc215e9e2e06363c22256777ce3009fe0
Gregory Quinio <quinio.gregory@gmail.com>
2021-03-26 09:39:28 +0000

    colors

--- colors/index.js
+++ colors/index.js
@@ -3,7 +3,16 @@ import windiColors from "windicss/colors";
 export const colors = {
   primary: "#5866CD",
   secondary: "#00FF7F",
-  black: "#171717",
-  background: "#F3F4F6",
-  ...windiColors,
+  black: {
+    default: windiColors.black,
+    divriots: windiColors.trueGray["900"],
+  },
+  gray: {
+    darker: "#262628",
+    dark: "#4d4d4f",
+    mid: "#737375",
+    light: "#99999B",
+    divriots: windiColors.coolGray["100"],
+  },
+  background: windiColors.coolGray["100"],
 };
