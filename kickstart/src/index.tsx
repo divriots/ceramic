fba19003a135dc2a468c1f0d8142c80b3da91f8e
Gregory Quinio <quinio.gregory@gmail.com>
2021-05-24 14:04:55 +0000

    placeholder kickstart

--- kickstart/src/index.tsx
+++ kickstart/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 import kits from '../../config/src/starter-kits';
+import { dropbox } from '../../config/src/commons';
 
 const StarterKit = ({ name, href, imgSrc }) => (
   <a href={href} key={name} className="inline-flex items-center p-2 gap-2">
@@ -11,7 +12,9 @@ const StarterKit = ({ name, href, imgSrc }) => (
 
 export default () => (
   <section className="p-4 sm:p-10 justify-center items-center flex flex-col md:flex-row bg-black-default text-white gap-8 md:gap-16">
-    <div className="max-w-md p-10 bg-secondary rounded-lg flex-grow grid justify-center"></div>
+    <div className="max-w-md p-10 bg-secondary rounded-lg flex-grow grid justify-center">
+      <img src={dropbox('kickstart.png')} />
+    </div>
     <div className="max-w-md text-left p-4">
       <h2 className="font-semibold mx-auto text-3xl">
         Kickstart your{' '}
