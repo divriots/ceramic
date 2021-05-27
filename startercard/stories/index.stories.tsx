cd9addef41b2ca2a75e7d09c17b7c01af633dd7c
muryoh <muryoh@gmail.com>
2021-05-28 00:18:29 +0200

    StarterCard component (#64)
    * StarterCard component
    
    Used in Highlight template and in the Backlight app
    Couldn't use @heroicons/vue because it's Vue 3 only
    There's a normal and big variant, static website uses normal
    while app will use the bigger one
    
    * Fix compat with astro
    
    Wasn't working with component directly, had to use <component>

---
+++ startercard/stories/index.stories.tsx
@@ -0,0 +1,30 @@
+import StarterCard from '../src/StarterCard.vue';
+import kits from '../../highlight/src/data.js';
+
+export default {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    components: { StarterCard },
+    template: `<StarterCard v-bind="props" actionDescription="Get started">{{ props.desc }}</StarterCard>`,
+  },
+};
+
+function props(name: string) {
+  return kits.find(kit => kit.name.toLowerCase().indexOf(name) >= 0);
+}
+
+export const normal = (args) => ({
+  setup() {
+    return { props: props("chakra") };
+  },
+  ...args,
+});
+
+export const big = (args) => ({
+  setup() {
+    return { props: {...props("tailwind"), size: 'big'} };
+  },
+  ...args,
+});
