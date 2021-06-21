3e8e40e86e9ba1160329b5357744d650b51f66ed
Georges Gomes <georges.gomes@gmail.com>
2021-06-21 12:53:12 +0000

    no links to starter kits

--- highlight/src/data.js
+++ highlight/src/data.js
@@ -5,9 +5,8 @@ export default [
     name: 'React',
     desc:
       'Design token in system-ui with examples in styled-components and theme-ui.',
-    url: '/edit/z99ptYZyvMOGDivUlP9u/',
-    bgImg:
-      'https://backlight.dev/assets/lib-white/react.svg',
+    url: '/request-earlyaccess',
+    bgImg: 'https://backlight.dev/assets/lib-white/react.svg',
   },
   {
     backgroundColor: '#30c6b9',
@@ -15,19 +14,16 @@ export default [
     name: 'Chakra',
     desc: `Chakra UI is a simple, modular and accessible component library. 
     Based on system-ui tokens.`,
-    url: '/edit/sNbJipmRTmslL3f8RZaO/',
-    bgImg:
-      'https://backlight.dev/assets/lib-white/react.svg',
+    url: '/request-earlyaccess',
+    bgImg: 'https://backlight.dev/assets/lib-white/react.svg',
   },
   {
     backgroundColor: '#06B6D4',
-    heroImg:
-      'https://backlight.dev/assets/lib-white/tailwindcss-typo.svg',
+    heroImg: 'https://backlight.dev/assets/lib-white/tailwindcss-typo.svg',
     name: 'Tailwindcss',
     desc: `Visualy setup tailwind's design tokens.
            Contains Tailwind headless-ui components for React and Vue.`,
-    url: '/edit/sNbJipmRTmslL3f8RZaO/',
-    bgImg:
-      'https://backlight.dev/assets/lib-white/css3.svg',
+    url: '/request-earlyaccess',
+    bgImg: 'https://backlight.dev/assets/lib-white/css3.svg',
   },
 ];
