96de01c58be706b41dd011c1e60184e13e815820
Mihai <mveres@users.noreply.github.com>
2021-09-03 18:19:46 +0300

    Atomic design: StarterCard breakdown (#108)
    * extracted empty card atom
    
    * starter card refactoring
    
    * starter card breakdown: atomic design
    
    * fix(version): backlight cli
    
    Co-authored-by: Gregory Quinio <1063530+gqio@users.noreply.github.com>

---
+++ starter-card/src/StarterCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <component
+    :is="EmptyCard"
+    :url="url"
+    @click="(event) => $emit('click', event)"
+    :backgroundColor="backgroundColor"
+    :size="size"
+  >
+    <component :is="StarterLogo" :name="name" :heroImg="heroImg" :size="size" />
+    <p class="description">
+      <slot></slot>
+    </p>
+    <component
+      :is="ActionDescription"
+      :description="actionDescription"
+      :size="size"
+    />
+    <img class="framework-bg" :src="bgImg" />
+  </component>
+</template>
+<script>
+import EmptyCard from '../../empty-card/src/EmptyCard.vue';
+import ActionDescription from '../../action-description/src/ActionDescription.vue';
+import StarterLogo from '../../starter-logo/src/StarterLogo.vue';
+
+export default {
+  props: {
+    name: { type: String },
+    url: { type: String },
+    href: { type: String },
+    heroImg: { type: String },
+    bgImg: { type: String },
+    backgroundColor: { type: String },
+    actionDescription: { type: String },
+    size: { type: String, default: 'normal' },
+  },
+  data: () => ({ EmptyCard, ActionDescription, StarterLogo }),
+};
+</script>
+<style lang="scss" scoped>
+.description {
+  flex-grow: 1;
+}
+
+.framework-bg {
+  position: absolute;
+  width: 14rem;
+  height: 14rem;
+  right: 0;
+  bottom: 0;
+  opacity: 0.25;
+  transform: translateX(4rem) translateY(3rem) rotate(-12deg);
+}
+</style>
