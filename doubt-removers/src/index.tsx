9e35f009d5c27bfddd6415e71534e25137e0380f
Charlene Grangy Kagita <charlenewebdev@gmail.com>
2021-09-08 13:08:36 +0000

    Coherence with the rest of the site fixed

--- doubt-removers/src/index.tsx
+++ doubt-removers/src/index.tsx
@@ -3,21 +3,17 @@ import content from './content';
 
 function DoubtRemovers() {
   return (
-    <section className="bg-black-divriots text-white px-4 py-12 -my-2">
-      <article className="overflow-hidden">
+    <section className="bg-black-divriots text-white px-8 py-12 -my-2 ">
+      <article className="md:px-20">
         <h3
           data-aos="fade-left"
           className="main-title text-center mx-12 my-10 sm:my-20"
           dangerouslySetInnerHTML={{ __html: content.data.title }}
         />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 py-24 px-12 gap-24">
+        <div className="max-w-6xl mx-auto grid md:grid-cols-2 place-items-center py-20 gap-12">
           {content.data.promises.map((promise, index: number) => (
-            <div
-              key={index}
-              data-aos="fade-up"
-              className="min-w-full mx-auto md:flex"
-            >
+            <div key={index} data-aos="fade-up" className="mx-auto md:flex">
               <div className=" w-16 h-16 md:w-20 md:h-20 md:mr-4 mb-8 md:mb-0">
                 <div
                   className="w-16 h-16 md:w-20 md:h-20"
