2cc816a2d46daecd8d3dbe3235f88333dd2c3f56
Charlene Grangy Kagita <charlenewebdev@gmail.com>
2021-09-08 10:38:54 +0000

    Final version Thais approved.

--- doubt-removers/src/index.tsx
+++ doubt-removers/src/index.tsx
@@ -11,14 +11,19 @@ function DoubtRemovers() {
           dangerouslySetInnerHTML={{ __html: content.data.title }}
         />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 py-12 px-12 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 py-24 px-12 gap-24">
           {content.data.promises.map((promise, index: number) => (
             <div
               key={index}
               data-aos="fade-up"
-              className="min-w-full mx-auto flex"
+              className="min-w-full mx-auto md:flex"
             >
-              <img src={promise.img.src} loading="lazy" className="w-20 h-20" />
+              <div className=" w-16 h-16 md:w-20 md:h-20 md:mx-4 mb-8 md:mb-0">
+                <div
+                  className="w-16 h-16 md:w-20 md:h-20"
+                  dangerouslySetInnerHTML={{ __html: promise.img.src }}
+                ></div>
+              </div>
 
               <div className="text-lg text-gray-divriots flex-col">
                 <span className="text-3xl font-semibold ">{promise.title}</span>
