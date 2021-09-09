d803320ff584f4dd61d023f92a700420f9a3a75e
Joren Broekema <joren.broekema@gmail.com>
2021-09-09 09:58:44 +0000

    feat: add left/right shades to Compatibility infini-scroll

--- compatibility/src/index.svelte
+++ compatibility/src/index.svelte
@@ -9,17 +9,19 @@
 <section class="text-center text-white bg-black-divriots px-8 pt-24 pb-16 relative">
   <h2 class="text-4xl sm:text-5xl lg:text-6xl">All Web technologies <span class="block text-primary">supported</span>
   </h2>
-  <infini-scroll class="mx-auto max-w-4xl my-32" drag-speed="1" scroll-interval="15" container-height="270"
-    box-width="128" row-amount="2" follow-user-direction>
-    {#each technologies as t, i}
-    <figure class="technology relative p-6">
-      <figcaption class="opacity-0 -bottom-2 w-full absolute text-center left-0">
-        <span class="p-1 text-white bg-black-divriots">{t.name}</span>
-      </figcaption>
-      <img class="w-20 h-20" src={t.logo} alt={t.name} aria-label={t.name} loading="lazy"/>
-    </figure>
-    {/each}
-  </infini-scroll>
+  <div class="infini-wrapper mx-auto max-w-4xl my-32">
+    <infini-scroll drag-speed="1" scroll-interval="15" container-height="270" box-width="128" row-amount="2"
+      follow-user-direction>
+      {#each technologies as t, i}
+      <figure class="technology relative p-6">
+        <figcaption class="opacity-0 -bottom-2 w-full absolute text-center left-0">
+          <span class="p-1 text-white bg-black-divriots">{t.name}</span>
+        </figcaption>
+        <img class="w-20 h-20" src={t.logo} alt={t.name} aria-label={t.name} loading="lazy"/>
+      </figure>
+      {/each}
+    </infini-scroll>
+  </div>
   <!-- <div class="absolute bottom-6 h-12 w-full bg-gradient-to-b from-transparent to-black-divriots z-index-1"></div> -->
 </section>
 
@@ -38,5 +40,31 @@
 
   .technology:hover figcaption {
     opacity: 1;
+  }
+
+  .infini-wrapper {
+    position: relative;
+  }
+
+  .infini-wrapper::before {
+    content: '';
+    position: absolute;
+    width: 150px;
+    inset: 0;
+    background: linear-gradient(to left, rgba(23, 23, 23, 0), rgba(23, 23, 23, 1));
+    z-index: 1;
+    pointer-events: none;
+  }
+
+  .infini-wrapper::after {
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 150px;
+    top: 0;
+    right: 0;
+    background: linear-gradient(to right, rgba(23, 23, 23, 0), rgba(23, 23, 23, 1));
+    z-index: 1;
+    pointer-events: none;
   }
 </style>
