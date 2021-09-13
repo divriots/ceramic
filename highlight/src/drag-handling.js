2d31d73fd804480c0a50803bead132da5ff797e2
Joren Broekema <joren.broekema@gmail.com>
2021-09-13 09:40:31 +0000

    chore: add drag-interaction threshold

--- highlight/src/drag-handling.js
+++ highlight/src/drag-handling.js
@@ -16,6 +16,7 @@ function mouseUpHandler() {
   // but mouseup is fired before click, so wait 1 render frame
   // before setting it back to false
   requestAnimationFrame(() => {
+    target.movedInX = 0;
     target.dragging = false;
   });
 }
@@ -28,18 +29,27 @@ function mouseDownHandler(ev) {
 
 function mouseMoveHandler(ev) {
   if (target.mouseDown && !target.dragging) {
+    target.movedInX = 0;
     target.dragging = true;
     dragHandler();
   }
   if (!target.dragging) {
     return;
   }
-  target.xDiff = ev.clientX - target.oldMousePosX;
+  const xDiff = ev.clientX - target.oldMousePosX;
+  // Keep track of how much has been moved in X direction
+  target.movedInX += Math.abs(xDiff);
+  target.xDiff = xDiff;
   target.oldMousePosX = ev.clientX;
 }
 
 function preventAnchorClickDuringDrag(ev) {
-  if (target.dragging) {
+  // If user was consciously dragging, prevent anchor visit
+  // The 5 here is the threshold for what is considered a conscious user "drag" interaction,
+  // the accumulated amount of pixels the user dragged to either side.
+  // If users complain about "click" not working, increase this threshold
+  // If users complain about "drag" accidentally visiting the anchor, lower it
+  if (target.dragging && target.movedInX > 5) {
     ev.preventDefault();
   }
 }
