3b27f9dca44a51c88fb7f23270b9b6399cf6c7c8
m4dz <code@m4dz.net>
2021-09-14 16:16:31 +0200

    Fix indent

--- price-info/src/index.vue
+++ price-info/src/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div :class="`price-info relative flex flex-col ${highlight? 'highlight' : 'lg:pt-12'}`">
-    <div class="flex bg-secondary rounded-md shadow-inset-control" v-if="highlight">
+  <div
+    :class="`price-info relative flex flex-col ${
+      highlight ? 'highlight' : 'lg:pt-12'
+    }`"
+  >
+    <div
+      class="flex bg-secondary rounded-md shadow-inset-control"
+      v-if="highlight"
+    >
       <button
         :class="`flex-grow btn focus:ring-offset-0 focus:ring-0 ${
           period === 'year' ? 'text-secondary bg-white' : ''
@@ -23,40 +30,43 @@
       <span class="flex flex-grow items-center tracking-tight font-medium">
         <template v-if="price && price.value >= 0">
           <span class="mr-2">{{ price.symbol }}</span>
-      <span aria-label="Subscription Price">{{ value * count }}</span>
-      <span class="ml-1 text-sm opacity-50 font-semibold tracking-wide">
+          <span aria-label="Subscription Price">{{ value * count }}</span>
+          <span class="ml-1 text-sm opacity-50 font-semibold tracking-wide">
             / month
           </span>
-</template>
-<span v-else class="text-lg mt-2">{{ price.value }}</span>
-</span>
-<div class="flex items-center" v-if="price && price.unit">
-  <div class="flex text-sm items-center" v-if="price && price.unit">
-    <button class="btn-secondary m-2 p-2" @click="count > 1 && count--">
+        </template>
+        <span v-else class="text-lg mt-2">{{ price.value }}</span>
+      </span>
+      <div class="flex items-center" v-if="price && price.unit">
+        <div class="flex text-sm items-center" v-if="price && price.unit">
+          <button class="btn-secondary m-2 p-2" @click="count > 1 && count--">
             -
           </button>
-    <span class="tabular-nums">{{ count }}</span>
-    <button class="btn-secondary m-2 p-2" @click="count++">+</button>
-  </div>
-  <span class="text-sm px-2">{{ price.unit }}</span>
-</div>
-</header>
-<p class="
+          <span class="tabular-nums">{{ count }}</span>
+          <button class="btn-secondary m-2 p-2" @click="count++">+</button>
+        </div>
+        <span class="text-sm px-2">{{ price.unit }}</span>
+      </div>
+    </header>
+    <p
+      class="
         bg-black-divriots
         text-white
         font-semibold
         text-center
         rounded-md
         py-1
-      " v-if="banner">
-  {{ banner }}
-</p>
-<span class="mx-auto">{{ legend }}</span>
-</div>
+      "
+      v-if="banner"
+    >
+      {{ banner }}
+    </p>
+    <span class="mx-auto">{{ legend }}</span>
+  </div>
 </template>
 
 <script>
-  import { ref } from 'vue';
+import { ref } from 'vue';
 export default {
   props: {
     title: { type: String },
@@ -85,20 +95,20 @@ export default {
 </script>
 
 <style>
-  .price-info {
-    min-height: 12.2rem;
-  }
+.price-info {
+  min-height: 12.2rem;
+}
 
-  .price-info.highlight {
-    min-height: 12rem;
-  }
+.price-info.highlight {
+  min-height: 12rem;
+}
 
-  .shadow-inset-control {
-    box-shadow: inset 0 0 2rem 2rem rgb(0 0 0 / 10%);
-    padding: 2px;
-  }
+.shadow-inset-control {
+  box-shadow: inset 0 0 2rem 2rem rgb(0 0 0 / 10%);
+  padding: 2px;
+}
 
-  .shadow-inset-control>button {
-    border-radius: 0.25rem;
-  }
+.shadow-inset-control > button {
+  border-radius: 0.25rem;
+}
 </style>
