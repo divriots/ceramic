3d0cb38a494087a8de47ed21034e2fb38f0f3b83
Charlene Grangy Kagita <charlenewebdev@gmail.com>
2021-09-14 21:00:49 +0000

    Conditional display of banner in CSS ONLY

--- product-hunt-bar/src/index.tsx
+++ product-hunt-bar/src/index.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
+import './style.css';
 
 function ProductHuntBar() {
-  const [isVisible, setIsVisible] = React.useState(true);
-
-  function hideBar() {
-    setIsVisible(false);
-  }
-
-  if (isVisible) {
-    return (
-      <a
-        href="https://www.producthunt.com/posts/backlight?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-backlight"
-        className="bg-primary text-white h-16 text-black-default flex justify-center items-center relative"
-      >
-        <p className="text-lg md:text-xl font-semibold mx-6">
-          <span className="md:mx-4">🎉</span>We are live on{' '}
-          <span className="underline">Product Hunt</span>!
-          <span className="md:mx-4">🎉</span>
-        </p>
+  return (
+    <div className="flex justify-center items-center relative">
+      <input type="checkbox" id="toggle" />
+      <label htmlFor="toggle" id="testLabel" className="absolute right-2">
         <svg
-          onClick={(event) => {
-            event.preventDefault();
-            hideBar();
-          }}
-          className="absolute right-2"
+          id="closeBanner"
           width="1.25rem"
           height="1.25rem"
           viewBox="0 0 16 16"
@@ -36,11 +20,21 @@ function ProductHuntBar() {
             d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
           />
         </svg>
+      </label>
+
+      <a
+        href="https://www.producthunt.com/posts/backlight?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-backlight"
+        id="banner"
+        className="flex justify-center items-center flex-grow bg-primary h-16 text-black-default controlled"
+      >
+        <p className="text-lg md:text-xl font-semibold mx-6">
+          <span className="md:mx-4">🎉</span>We are live on{' '}
+          <span className="underline">Product Hunt</span>!
+          <span className="md:mx-4">🎉</span>
+        </p>
       </a>
-    );
-  } else {
-    return null;
-  }
+    </div>
+  );
 }
 
 export default ProductHuntBar;
