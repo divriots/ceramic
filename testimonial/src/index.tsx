57ecaca33e7a2b62fc3b522b22117700e6dcc07a
Charlene Grangy Kagita <charlene@divriots.com>
2021-10-12 13:33:50 +0000

    Testimonial section with horizontal scroll on mobile

--- testimonial/src/index.tsx
+++ testimonial/src/index.tsx
@@ -1,74 +1,120 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TestimonialCard from '../../testimonial-card/src/index';
+import './styles.scss';
+import { setupScrollArrows } from './scroll-arrows.js';
+import { setupDragHandling } from './drag-handling.js';
 
 function Testimonial({ title, subtitle, testimonials }) {
+  useEffect(() => {
+    setupScrollArrows();
+    setupDragHandling();
+  }, []);
   return (
-    <section className="bg-black-divriots text-white leading-normal text-base sm:text-lg px-8 py-12 -my-2">
-      <article className="place-items-center lg:grid lg:grid-cols-2 lg:w-920 m-auto">
-        {/*Column 1*/}
-        <div className="col-auto">
-          <div className="relative">
-            {/*Big quote mark*/}
-            <svg
-              width="143"
-              height="120"
-              viewBox="0 0 143 120"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute"
-            >
-              <g opacity="0.3" clipPath="url(#clip0)">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M142.373 18.7419C121.049 29.1541 110.387 41.3882 110.387 55.4447C119.476 56.4859 126.992 60.1735 132.934 66.5076C138.877 72.8417 141.849 80.1735 141.849 88.5033C141.849 97.3536 138.965 104.816 133.197 110.889C127.428 116.963 120.175 120 111.435 120C101.647 120 93.1701 116.052 86.0037 108.156C78.8374 100.26 75.2542 90.6725 75.2542 79.3926C75.2542 45.553 94.306 19.089 132.41 0L142.373 18.7419ZM67.1186 18.7419C45.6196 29.1541 34.8702 41.3882 34.8702 55.4447C44.134 56.4859 51.7373 60.1735 57.6801 66.5076C63.6229 72.8417 66.5943 80.1735 66.5943 88.5033C66.5943 97.3536 63.6666 104.816 57.8112 110.889C51.9557 116.963 44.6584 120 35.919 120C26.1308 120 17.6973 116.052 10.6184 108.156C3.53942 100.26 0 90.6725 0 79.3926C0 45.553 18.9643 19.089 56.8935 0L67.1186 18.7419Z"
-                  fill="#008594"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0">
-                  <rect width="143" height="120" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-          </div>
-          <div className="lg:ml-24">
-            {/*Title and subtitle*/}
-            <div className="relative z-10 pt-16 lg:w-96">
-              <h1 className="text-4xl sm:text-5xl">{title}</h1>
-              <p className="my-4">{subtitle}</p>
-            </div>
+    <section className="bg-black-divriots overflow-hidden text-white leading-normal text-base sm:text-lg px-8 py-12 -my-2 flex justify-center">
+      <div className="w-full lg:w-auto">
+        <div className="relative lg:w-920">
+          {/*Big quote mark*/}
+          <svg
+            width="143"
+            height="120"
+            viewBox="0 0 143 120"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute"
+          >
+            <g opacity="0.3" clipPath="url(#clip0)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M142.373 18.7419C121.049 29.1541 110.387 41.3882 110.387 55.4447C119.476 56.4859 126.992 60.1735 132.934 66.5076C138.877 72.8417 141.849 80.1735 141.849 88.5033C141.849 97.3536 138.965 104.816 133.197 110.889C127.428 116.963 120.175 120 111.435 120C101.647 120 93.1701 116.052 86.0037 108.156C78.8374 100.26 75.2542 90.6725 75.2542 79.3926C75.2542 45.553 94.306 19.089 132.41 0L142.373 18.7419ZM67.1186 18.7419C45.6196 29.1541 34.8702 41.3882 34.8702 55.4447C44.134 56.4859 51.7373 60.1735 57.6801 66.5076C63.6229 72.8417 66.5943 80.1735 66.5943 88.5033C66.5943 97.3536 63.6666 104.816 57.8112 110.889C51.9557 116.963 44.6584 120 35.919 120C26.1308 120 17.6973 116.052 10.6184 108.156C3.53942 100.26 0 90.6725 0 79.3926C0 45.553 18.9643 19.089 56.8935 0L67.1186 18.7419Z"
+                fill="#008594"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0">
+                <rect width="143" height="120" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
 
-            {/*Testimonial 1*/}
-            <TestimonialCard
-              text={testimonials[0].text}
-              author={testimonials[0].author}
-              author_title={testimonials[0].author_title}
-            />
-            {/*Testimonial 2*/}
-            <TestimonialCard
-              text={testimonials[1].text}
-              author={testimonials[1].author}
-              author_title={testimonials[1].author_title}
-            />
+          {/*Title and subtitle*/}
+          <div className="relative z-10 pt-16 max-w-96 sm:w-96 lg:ml-24">
+            <h1 className="text-4xl sm:text-5xl">{title}</h1>
+            <p className="my-4">{subtitle}</p>
           </div>
         </div>
-        {/*Column 2*/}
-        <div className="col-auto lg:mt-20">
-          {/*Testimonial 3*/}
-          <TestimonialCard
-            text={testimonials[2].text}
-            author={testimonials[2].author}
-            author_title={testimonials[2].author_title}
-          />
-          {/*Testimonial 4*/}
-          <TestimonialCard
-            text={testimonials[3].text}
-            author={testimonials[3].author}
-            author_title={testimonials[3].author_title}
-          />
+
+        <div className="wrapper">
+          <svg
+            className="left-paddle lg:hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M5.928 7.976l4.357 4.357-.618.62L5 8.284v-.618L9.667 3l.618.619-4.357 4.357z"
+            />
+          </svg>
+          <article className="flex lg:grid lg:grid-cols-2 lg:w-920 overflow-x-scroll lg:overflow-visible">
+            {/*Column 1*/}
+            <div className="flex lg:block lg:ml-24">
+              {/*Testimonial 1*/}
+              <div className="w-64">
+                <TestimonialCard
+                  text={testimonials[0].text}
+                  author={testimonials[0].author}
+                  author_title={testimonials[0].author_title}
+                />
+              </div>
+              {/*Testimonial 2*/}
+              <div className="mx-4 lg:mx-0 w-64">
+                <TestimonialCard
+                  text={testimonials[1].text}
+                  author={testimonials[1].author}
+                  author_title={testimonials[1].author_title}
+                />
+              </div>
+            </div>
+            {/*Column 2*/}
+            <div className="flex lg:block lg:-mt-48 lg:ml-12">
+              {/*Testimonial 3*/}
+              <div className="mr-4 lg:mr-0 w-64">
+                <TestimonialCard
+                  text={testimonials[2].text}
+                  author={testimonials[2].author}
+                  author_title={testimonials[2].author_title}
+                />
+              </div>
+              {/*Testimonial 4*/}
+              <div className="w-64">
+                <TestimonialCard
+                  text={testimonials[3].text}
+                  author={testimonials[3].author}
+                  author_title={testimonials[3].author_title}
+                />
+              </div>
+            </div>
+          </article>
+          <svg
+            className="right-paddle lg:hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.072 8.024L5.715 3.667l.618-.62L11 7.716v.618L6.333 13l-.618-.619 4.357-4.357z"
+            />
+          </svg>
         </div>
-      </article>
+      </div>
     </section>
   );
 }
--- testimonial/stories/index.stories.tsx
+++ testimonial/stories/index.stories.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import Testimonial from '../src/index';
 import '~/tailwind';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-
-AOS.init();
 
 export const story1 = () => (
   <Testimonial
@@ -13,27 +9,25 @@ export const story1 = () => (
     subtitle="Get inspired by these stories."
     testimonials={[
       {
-        text: 'The next generation of tooling to help teams build, ship, and evolve design systems.',
-        author: 'Kaelig Deloumeau-Prigent',
-        author_title:
-          'Developer tooling at Shopify, and co-chair of the Design Tokens W3C Community Group',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit nisl tortor, ac sodales justo pellentesque sed.',
+        author: 'Lorem ipsum dolor',
+        author_title: 'Lorem ipsum dolor sit amet',
       },
       {
-        text: 'I have been following what those guys are doing since the early days, and the promise of Backlight completely sells me. [...] Thanks to Backlight for easing our product team collaboration at Qovery. Backlight is a must-have for all product-driven companies and teams. I recommend.',
-        author: 'Romaric Philogène',
-        author_title: 'CEO, co-founder and product developer of Qovery',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit nisl tortor, ac sodales justo pellentesque sed. Integer efficitur viverra lacus, eget condimentum mauris dictum ut. Nulla a semper eros. Curabitur eget condimentum urna. Aliquam sit amet lectus vehicula, mattis nibh quis, vestibulum odio. Cras posuere sodales mauris, ac aliquam dui ornare et. Curabitur tincidunt elit a purus elementum iaculis. Cras egestas dictum orci vitae vestibulum. Sed quis lacus lectus.',
+        author: 'Lorem ipsum dolor',
+        author_title: 'Lorem ipsum dolor sit amet',
       },
       {
-        text: 'We’ve had the privilege to deep dive into <div>Riots’ Backlight in private beta for the past 6 months. Not only does the platform save our teams a crucial time in terms of Design System development, but this best-in-class solution enables designers and developers to smoothly collaborate. Collaboration in web design has been a pain point for many years and we are excited to see innovation in the space.',
-        author: 'Olivier Grandhomme',
-        author_title: 'CTO at Saqara',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit nisl tortor, ac sodales justo pellentesque sed. Integer efficitur viverra lacus, eget condimentum mauris dictum ut. Nulla a semper eros. Curabitur eget condimentum urna. Aliquam sit amet lectus vehicula, mattis nibh quis, vestibulum odio. Cras posuere sodales mauris, ac aliquam dui ornare et. Curabitur tincidunt elit a purus elementum iaculis. Cras egestas dictum orci vitae vestibulum. Sed quis lacus lectus.',
+        author: 'Lorem ipsum dolor',
+        author_title: 'Lorem ipsum dolor sit amet',
       },
       {
-        text: 'Woow. As a seasoned frontend engineer I really liked it. Design System is essential and had to be put from the start. Later on the development it is really hard to adopt one. I will keep looking after Backlight. Good job team!',
-        author: 'Mustafa Ilker Sarac',
-        author_title: 'Engineer, Thundraio',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit nisl tortor, ac sodales justo pellentesque sed. Integer efficitur viverra lacus, eget condimentum mauris dictum ut.',
+        author: 'Lorem ipsum dolor',
+        author_title: 'Lorem ipsum dolor sit amet',
       },
     ]}
   ></Testimonial>
 );
-
