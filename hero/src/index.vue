de2e7380e8438678cd17c1fbc11e1d7708ad37f9
Gregory Quinio <1063530+gqio@users.noreply.github.com>
2022-02-07 21:16:32 +0100

    Improve api.video controls (#220)
    * - hide all controls
    - show volume and progressbar
    - start muted
    
    * Push from hiddenControls
    
    Co-authored-by: Gregory Quinio <1063530+undefined@users.noreply.github.com>

--- hero/src/index.vue
+++ hero/src/index.vue
@@ -47,9 +47,13 @@ export default {
           apiScript.onload = () => {
             const sdk = new PlayerSdk('#hero-embedded-placeholder', {
               id: videoSrc,
+              hideControls: true,
               // ... other optional options, see https://docs.api.video/docs/video-player-sdk#method-2-typescript
             });
-            sdk.setVolume(0.1);
+            sdk.mute();
+            sdk.addEventListener('play', () =>
+              sdk.showControls(['volume', 'progressBar'])
+            );
             resolve(sdk);
           };
         });
