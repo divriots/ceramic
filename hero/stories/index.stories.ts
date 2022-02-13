2f2f8d5b8a77ecf8b24a3e8694b2383164b42a56
Georges Gomes <georges@divriots.com>
2022-02-13 09:20:48 +0100

    Fix cls (#231)
    * Fix cls
    
    * Fix play button position
    
    Co-authored-by: Georges Gomes <5250572+undefined@users.noreply.github.com>

--- hero/stories/index.stories.ts
+++ hero/stories/index.stories.ts
@@ -3,22 +3,23 @@ import '~/tailwind';
 
 export default {
   parameters: {
+    backgrounds: { default: 'black' },
     layout: 'fullscreen',
   },
 };
 
 export const localVideo = () => ({
   components: { Hero },
-  template: `<Hero videoSrc="https://user-images.githubusercontent.com/5250572/131305534-6e4baa97-8cb1-4e45-b14c-15600f232167.mp4" imgSrc="/img/landing/hero.png"/>`,
+  template: `<Hero videoSrc="https://user-images.githubusercontent.com/5250572/131305534-6e4baa97-8cb1-4e45-b14c-15600f232167.mp4" imgSrc="/img/landing/hero.png" imgWidth="1280" imgHeight="784"/>`,
 });
 
 export const youtubeVideo = () => ({
   components: { Hero },
-  template: `<Hero videoType="youtube" videoSrc="veDoIqU2So0" imgSrc="/img/landing/hero.png"/>`,
+  template: `<Hero videoType="youtube" videoSrc="veDoIqU2So0" imgSrc="/img/landing/hero.png" imgWidth="1280" imgHeight="784"/>`,
 });
 
 //The id here is in a sandbox, will lead to nothing in 24 hours
 export const apiVideo = () => ({
   components: { Hero },
-  template: `<Hero videoType="apivideo" videoSrc="vi5SjuJEAtvnXFOSMzCp5gxC" imgSrc="/img/landing/hero.png"/>`,
+  template: `<Hero videoType="apivideo" videoSrc="vi5SjuJEAtvnXFOSMzCp5gxC" imgSrc="/img/landing/hero.png" imgWidth="1280" imgHeight="784"/>`,
 });
