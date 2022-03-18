07c099b818c955b9818465c28fc7acd1c7462fa3
Georges Gomes <5250572+undefined@users.noreply.github.com>
2022-03-18 13:26:21 +0000

    Fix hero size

--- hero/stories/index.stories.ts
+++ hero/stories/index.stories.ts
@@ -10,15 +10,15 @@ export default {
 
 export const localVideo = () => ({
   components: { Hero },
-  template: `<Hero videoSrc="https://user-images.githubusercontent.com/5250572/131305534-6e4baa97-8cb1-4e45-b14c-15600f232167.mp4" imgSrc="/img/landing/hero.png" imgWidth="1280" imgHeight="784"/>`,
+  template: `<Hero videoSrc="https://user-images.githubusercontent.com/5250572/131305534-6e4baa97-8cb1-4e45-b14c-15600f232167.mp4" imgSrc="/img/landing/Landing_Hero_Illustration.webp" imgWidth="2396" imgHeight="1590"/>`,
 });
 
 export const youtubeVideo = () => ({
   components: { Hero },
-  template: `<Hero videoType="youtube" videoSrc="veDoIqU2So0" imgSrc="/img/landing/hero.png" imgWidth="1280" imgHeight="784"/>`,
+  template: `<Hero videoType="youtube" videoSrc="veDoIqU2So0" imgSrc="/img/landing/Landing_Hero_Illustration.webp" imgWidth="1280" imgHeight="784"/>`,
 });
 
 export const apiVideo = () => ({
   components: { Hero },
-  template: `<Hero videoType="apivideo" videoSrc="vi5SjuJEAtvnXFOSMzCp5gxC" imgSrc="/img/landing/hero.png" highRes="/img/landing/designlink.png 2x" imgWidth="1280" imgHeight="784"/>`,
+  template: `<Hero videoType="apivideo" videoSrc="vi5SjuJEAtvnXFOSMzCp5gxC" imgSrc="/img/landing/Landing_Hero_Illustration.webp" highRes="/img/landing/designlink.png 2x" imgWidth="1280" imgHeight="784"/>`,
 });
