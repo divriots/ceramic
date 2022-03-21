a1ddb6240b4e63834a771a92799c9ecdfceba143
Joren Broekema <joren.broekema@gmail.com>
2022-03-21 10:20:50 +0100

    chore: add rev-tokens in LCD stories (#271)
    Co-authored-by: Joren Broekema <36734656+undefined@users.noreply.github.com>

---
+++ stories.preview.tsx
@@ -0,0 +1,11 @@
+/**
+ * Load our Rev CSS custom properties
+ * for all our stories.
+ * Note that in the final app, the app is
+ * responsible for doing this step,
+ * not the component, as this is global styles.
+ *
+ * Since we are still on MTC, we need the raw path,
+ * when we move to Vite, use @divriots/rev/tokens.css
+ */
+import '@divriots/rev/tokens/tokens.css';
